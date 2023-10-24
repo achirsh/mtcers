94c5482a2655281a0dde2f510a14a2a6cd33514a
geqb <geqb@shanhai.team>
2023-10-24 21:42:08 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // import { visualizer } from 'rollup-plugin-visualizer';
-import viteCompression from 'vite-plugin-compression'
+// import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({
-      //gzip压缩
-      deleteOriginFile: true, // 删除源文件
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz'
-    }),
+    // viteCompression({
+    //   //gzip压缩
+    //   deleteOriginFile: true, // 删除源文件
+    //   verbose: true,
+    //   disable: false,
+    //   threshold: 10240,
+    //   algorithm: 'gzip',
+    //   ext: '.gz'
+    // }),
     // visualizer()
   ],
 })
