6dc448a2e417cb0630f9f7d941566b181d733d6a
geqb <geqb@shanhai.team>
2023-10-24 21:16:00 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import viteCompression from 'vite-plugin-compression'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression()
   ],
 })
